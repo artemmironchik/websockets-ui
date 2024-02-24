2bf0873c5178133b8bdb9f0d9ff3a232124b224a
Artem Mironchik <tawerka.power@gmail.com>
2024-02-24 18:24:52 +0300

    init: start task

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+import "dotenv/config"
 import { httpServer } from "./src/http_server/index.js";
 
-const HTTP_PORT = 8181;
+const HTTP_PORT = process.env.PORT;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
